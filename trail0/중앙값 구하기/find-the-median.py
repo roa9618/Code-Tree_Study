d6534832fed0fdb15b0e97b69bdea12b04ba0009
Woohyeon Roh <ykjrco@naver.com>
2026-07-16 05:12:15 +0900

    [Passed] 중앙값 구하기 (59ms, 16MB)

--- trail0/중앙값 구하기/find-the-median.py
+++ trail0/중앙값 구하기/find-the-median.py
@@ -1,4 +1,8 @@
-num = list(map(int, input().split()))
-num = sorted(num)
+a, b, c = map(int, input().split())
 
-print(num[1])
+if (a >= b and a <= c) or (a <= b and a >= c):
+    print(a)
+elif (b >= a and b<= c) or (b <= a and b >= c):
+    print(b)
+else:
+    print(c)
